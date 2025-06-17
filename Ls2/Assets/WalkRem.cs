3ba84241d9fe449b026a43099099f3808b173cfe
IceTeaSS69 <dima.kuzin.1010@gmail.com>
2025-06-17 21:30:59 +0300

    Added Starfall game

---
+++ Ls2/Assets/WalkRem.cs
@@ -0,0 +1,71 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class WalkRem : MonoBehaviour
+{
+
+    [SerializeField]
+ 
+    public int Coins = 0;
+    public float WalkL = 0.5f;
+    public int Health = 100;
+    public Text Record;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+    private void Awake()
+    {
+        
+
+
+
+
+    }
+    public void OnCollisionEnter2D(Collision2D collision)
+    { 
+        if (collision.gameObject.tag == "Coin")
+        {
+            Destroy(collision.gameObject);
+            Coins++;
+            Record.text = ($"{Coins}");
+            Health--;
+        }
+    }
+    public void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "Coin")
+        {          
+            Destroy(collider.gameObject);
+            Coins++;
+            Record.text = ($"{Coins}");
+            Health--;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            gameObject.transform.position = gameObject.transform.position - new Vector3(WalkL, 0,0);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            gameObject.transform.position = gameObject.transform.position + new Vector3(WalkL, 0, 0);
+        }
+
+
+    }
+    public void HealthAndStamina()
+    {
+        if(Health >= 0)
+        {
+            Destroy(gameObject);
+            Record.text = ($"{Coins}");
+        }
+    }
+}
