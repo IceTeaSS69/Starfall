56daa0ff434ce4cab9108192b9bf6c3963cb1c04
IceTeaSS69 <dima.kuzin.1010@gmail.com>
2025-06-19 19:45:17 +0300

    Added *Score Screen*

--- Ls2/Assets/Plat.cs
+++ Ls2/Assets/Plat.cs
@@ -6,5 +6,48 @@ using static UnityEngine.InputSystem.LowLevel.InputStateHistory;
 
 public class Plat : MonoBehaviour
 {
-    
+    public GameObject player;
+    public Text Scoreline;
+
+    private float DelCircles = 0;
+    private void Update()
+    {
+        if (player == null)
+        {
+            // Показываем текст
+            Scoreline.text = ($"Your Record/{DelCircles}");
+            Time.timeScale = 0;
+        }
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Coin")
+        {
+            Destroy(collision.gameObject);
+            DelCircles++;
+        }
+        if (collision.gameObject.tag == "Platform")
+        {
+            Destroy(collision.gameObject);
+            DelCircles++;
+        }
+
+
+    }
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "Coin")
+        {
+            Destroy(collider.gameObject);
+            DelCircles++;
+        }
+        if (collider.gameObject.tag == "Platform")
+        {
+            Destroy(collider.gameObject);
+            DelCircles++;
+        }
+
+    }
 }
